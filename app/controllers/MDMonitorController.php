1f6a930d90241bd2904ef9d46205c760e23b6668
zino <smile.zino@gmail.com>
2014-01-10 16:51:50 +0800

    代码优化

--- app/controllers/MDMonitorController.php
+++ app/controllers/MDMonitorController.php
@@ -25,17 +25,21 @@ class MDMonitorController extends RESTController{
 		$this->log("[get] origin ip->".$data['origin_ip']);
 		$this->log("[get] param ip->".$data['param_ip']);
 		
-		if(!$this->is_valid_ip($data['param_ip'])){
-			$data['ip'] = $data['origin_ip'];
-		}else{
-			if(MAD_MONITOR_IP_CHECK){
-				if($this->existIp($data['origin_ip'])) {
+		if(MAD_MONITOR_IP_CHECK){
+			if($this->existIp($data['origin_ip'])) {
+				if($this->is_valid_ip($data['param_ip'])){
 					$data['ip'] = $data['param_ip'];
 				}else{
 					$data['ip'] = $data['origin_ip'];
 				}
 			}else{
+				$data['ip'] = $data['origin_ip'];
+			}
+		}else{
+			if($this->is_valid_ip($data['param_ip'])){
 				$data['ip'] = $data['param_ip'];
+			}else{
+				$data['ip'] = $data['origin_ip'];
 			}
 		}
 	
