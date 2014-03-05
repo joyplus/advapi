2347db04c3f748a7452476e2fb286bb21f579990
zino <smile.zino@gmail.com>
2014-03-05 11:15:09 +0800

    #106

---
+++ app/models/DevicePackageMatrix.php
@@ -0,0 +1,20 @@
+<?php
+class DevicePackageMatrix extends BaseModel
+{
+
+    public $id;
+    public $device_id;
+    public $package_id;
+
+    public function initialize() {
+        $this->setReadConnectionService('dbSlave');
+        $this->setWriteConnectionService('dbMaster');
+        $this->useDynamicUpdate(true);
+    }
+
+    // 获取数据库表名
+    public function getSource() {
+        return "md_device_package_matrix";
+    }
+
+}
