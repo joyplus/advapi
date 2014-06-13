45ac78724733ca8fbbf3c9ed6261e396e3613fa5
zino <smile.zino@gmail.com>
2014-06-13 10:31:43 +0800

    #182

---
+++ app/routes/collections/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return call_user_func(function($config){
+
+	$collection = new \Phalcon\Mvc\Micro\Collection();
+
+	$collection->setHandler('MDConfigController')->setLazy(true);
+
+    
+    $collection->get('/'.$config->application->mdtime, 'timestamp');
+
+	return $collection;
+}, $config);
