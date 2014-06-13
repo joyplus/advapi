8436cf96785d6feafc189b62bbd5fbc2c712479b
zino <smile.zino@gmail.com>
2014-06-13 14:24:36 +0800

    qiniu sdk

--- app/controllers/MDConfigController.php
+++ app/controllers/MDConfigController.php
@@ -2,7 +2,15 @@
 
 class MDConfigController extends RESTController{
 	public function timestamp() {
-		$data = "<timestamp>".time()."</timestamp>";
+		include_once __DIR__ . '/../modules/qiniu/rs.php';
+		
+		Qiniu_SetKeys(QINIU_ACCESS_KEY, QINIU_SECRET_KEY);
+		$putPolicy = new Qiniu_RS_PutPolicy(QINIU_PREUPLOAD_BUKECT);
+		$upToken = $putPolicy->Token(null);
+		$data = "<preupload>";
+		$data .= "<timestamp>".time()."</timestamp>";
+		$data .= "<uptoken>".$upToken."</uptoken>";
+		$data .= "</preupload>";
 		$this->outputXml($data);
 	}
 }
